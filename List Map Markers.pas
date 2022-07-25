676a95e3eab6bd65e1b882cf3aa739ccb7f4a1a4
FloorBelow <floorbelow@zoho.com>
2022-07-26 03:28:19 +1200

    initial

---
+++ List Map Markers.pas
@@ -0,0 +1,37 @@
+unit ListMapMarkers;
+
+function Initialize: integer;
+var
+  MapMarkerStat, e, markerParent, flags: IInterface;
+  i, j: integer;
+  s, flagStr, loc: string;
+begin
+    MapMarkerStat := RecordByFormID(FileByIndex(0), $00000010, False);
+    for i := 0 to Pred(ReferencedByCount(MapMarkerStat)) do begin
+		e := ReferencedByIndex(MapMarkerStat, i);
+		if Signature(e) = 'REFR' then begin
+			s := GetElementEditValues(e, 'Map Marker\FULL');
+			if GetElementNativeValues(e, 'Map Marker\FNAM') and 8 > 0 then begin
+				for j := 0 to Pred(ReferencedByCount(e)) do begin
+					markerParent := ReferencedByIndex(e, j);
+					if Signature(markerParent) = 'LCTN' then begin
+						loc := GetElementEditValues(markerParent, 'FULL');
+						if s = '' then
+							s := loc
+						else if loc <> s then
+							s := loc + ' (' + s + ')';
+						Break;
+					end;
+				end;
+			end;
+
+			//if GetIsInitiallyDisabled(e) then
+			//	s := s + ' (DISABLED)';
+			if IntToHex(FormID(LinksTo(ElementByName(LinksTo(ElementByName(e, 'Cell')), 'Worldspace'))), 6) = '25DA15' then
+				if s <> '' then
+					AddMessage(IntToHex(FixedFormID(e), 6) + ' ' + s);
+		end;
+	end;
+end;
+
+end.
